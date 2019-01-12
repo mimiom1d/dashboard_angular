69ed26f0ec4053da5a406e9478553297d903c436
Mim M <mimi1design@gmail.com>
2019-01-11 23:45:03 -0500

    added router and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+//** Modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router'; //** for navigation */
 
+//** Components */
 import { AppComponent } from './app.component';
-import { HomeComponent} from './pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
+//** Custome Routes to pass to RouterModule */
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
